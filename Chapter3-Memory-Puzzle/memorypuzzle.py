2239a40828a7ccb7183d0e1dd3158efaa3b076dd
ichn <ichneumon@outlook.com>
2016-08-25 23:20:41 +0800

    prettify

--- Chapter3-Memory-Puzzle/memorypuzzle.py
+++ Chapter3-Memory-Puzzle/memorypuzzle.py
@@ -18,16 +18,17 @@ assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an evennumber o
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
 
-GRAY = (100, 100, 100)
-NAVYBLUE = ( 60, 60, 100)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = ( 0, 255, 0)
-BLUE = ( 0, 0, 255)
-YELLOW = (255, 255, 0)
-ORANGE = (255, 128, 0)
-PURPLE = (255, 0, 255)
-CYAN = ( 0, 255, 255)
+#             R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
 BGCOLOR = NAVYBLUE
 LIGHTBGCOLOR = GRAY
 BOXCOLOR = WHITE
