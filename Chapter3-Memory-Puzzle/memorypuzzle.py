6216f2a198ad1883d1a28f516653a71108dbd251
ichn <ichneumon@outlook.com>
2016-08-25 23:15:20 +0800

    prod2

--- Chapter3-Memory-Puzzle/memorypuzzle.py
+++ Chapter3-Memory-Puzzle/memorypuzzle.py
@@ -151,74 +151,64 @@ def getBoxAtPixel(x, y):
 		return (boxx, boxy)
 	return (None, None)
 def drawIcon(shape, color, boxx, boxy):
- quarter = int(BOXSIZE * 0.25) # syntactic sugar
- half =
- int(BOXSIZE * 0.5) # syntactic sugar
- left, top = leftTopCoordsOfBox(boxx, boxy) # get pixel coords from board coords
- # Draw the shapes
- if shape == DONUT:
- pygame.draw.circle(DISPLAYSURF, color, (left + half, top + half), half - 5)
- pygame.draw.circle(DISPLAYSURF, BGCOLOR, (left + half, top +
-half), quarter - 5)
- elif shape == SQUARE:
- pygame.draw.rect(DISPLAYSURF, color, (left + quarter, top +
-quarter, BOXSIZE - half, BOXSIZE - half))
- elif shape == DIAMOND:
- pygame.draw.polygon(DISPLAYSURF, color, ((left + half, top), (left
-+ BOXSIZE - 1, top + half), (left + half, top + BOXSIZE - 1), (left, top +
-half)))
- elif shape == LINES:
- for i in range(0, BOXSIZE, 4):
- pygame.draw.line(DISPLAYSURF, color, (left, top + i), (left +
-i, top))
- pygame.draw.line(DISPLAYSURF, color, (left + i, top + BOXSIZE
-- 1), (left + BOXSIZE - 1, top + i))
- elif shape == OVAL:
- pygame.draw.ellipse(DISPLAYSURF, color, (left, top + quarter,
-BOXSIZE, half))
+	quarter = int(BOXSIZE * 0.25) # syntactic sugar
+	half = int(BOXSIZE * 0.5) # syntactic sugar
+	left, top = leftTopCoordsOfBox(boxx, boxy) # get pixel coords from board coords
+	# Draw the shapes
+	if shape == DONUT:
+		pygame.draw.circle(DISPLAYSURF, color, (left + half, top + half), half - 5)
+		pygame.draw.circle(DISPLAYSURF, BGCOLOR, (left + half, top + half), quarter - 5)
+	elif shape == SQUARE:
+		pygame.draw.rect(DISPLAYSURF, color, (left + quarter, top + quarter, BOXSIZE - half, BOXSIZE - half))
+	elif shape == DIAMOND:
+		pygame.draw.polygon(DISPLAYSURF, color, ((left + half, top), (left + BOXSIZE - 1, top + half), (left + half, top + BOXSIZE - 1), (left, top + half)))
+	elif shape == LINES:
+	for i in range(0, BOXSIZE, 4):
+		pygame.draw.line(DISPLAYSURF, color, (left, top + i), (left + i, top))
+		pygame.draw.line(DISPLAYSURF, color, (left + i, top + BOXSIZE - 1), (left + BOXSIZE - 1, top + i))
+	elif shape == OVAL:
+		pygame.draw.ellipse(DISPLAYSURF, color, (left, top + quarter, BOXSIZE, half))
 def getShapeAndColor(board, boxx, boxy):
  # shape value for x, y spot is stored in board[x][y][0]
  # color value for x, y spot is stored in board[x][y][1]
- return board[boxx][boxy][0], board[boxx][boxy][1]
+	return board[boxx][boxy][0], board[boxx][boxy][1]
 def drawBoxCovers(board, boxes, coverage):
  # Draws boxes being covered/revealed. "boxes" is a list
  # of two-item lists, which have the x & y spot of the box.
- for box in boxes:
- left, top = leftTopCoordsOfBox(box[0], box[1])
- pygame.draw.rect(DISPLAYSURF, BGCOLOR, (left, top, BOXSIZE,
-BOXSIZE))
- shape, color = getShapeAndColor(board, box[0], box[1])
- drawIcon(shape, color, box[0], box[1])
- if coverage > 0: # only draw the cover if there is an coverage
- pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, coverage,
-BOXSIZE))
- pygame.display.update()
- FPSCLOCK.tick(FPS)
+	for box in boxes:
+		left, top = leftTopCoordsOfBox(box[0], box[1])
+		pygame.draw.rect(DISPLAYSURF, BGCOLOR, (left, top, BOXSIZE, BOXSIZE))
+		shape, color = getShapeAndColor(board, box[0], box[1])
+		drawIcon(shape, color, box[0], box[1])
+	if coverage > 0: # only draw the cover if there is an coverage
+		pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, coverage, BOXSIZE))
+		pygame.display.update()
+		FPSCLOCK.tick(FPS)
 def revealBoxesAnimation(board, boxesToReveal):
- # Do the "box reveal" animation.
- for coverage in range(BOXSIZE, (-REVEALSPEED) - 1, - REVEALSPEED):
- drawBoxCovers(board, boxesToReveal, coverage)
+	# Do the "box reveal" animation.
+	for coverage in range(BOXSIZE, (-REVEALSPEED) - 1, - REVEALSPEED):
+		drawBoxCovers(board, boxesToReveal, coverage)
 def coverBoxesAnimation(board, boxesToCover):
- # Do the "box cover" animation.
- for coverage in range(0, BOXSIZE + REVEALSPEED, REVEALSPEED):
- drawBoxCovers(board, boxesToCover, coverage)
+	# Do the "box cover" animation.
+	for coverage in range(0, BOXSIZE + REVEALSPEED, REVEALSPEED):
+		drawBoxCovers(board, boxesToCover, coverage)
+
 def drawBoard(board, revealed):
- # Draws all of the boxes in their covered or revealed state.
- for boxx in range(BOARDWIDTH):
- for boxy in range(BOARDHEIGHT):
- left, top = leftTopCoordsOfBox(boxx, boxy)
- if not revealed[boxx][boxy]:
- # Draw a covered box.
- pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top,
-BOXSIZE, BOXSIZE))
- else:
- # Draw the (revealed) icon.
- shape, color = getShapeAndColor(board, boxx, boxy)
- drawIcon(shape, color, boxx, boxy)
+	# Draws all of the boxes in their covered or revealed state.
+	for boxx in range(BOARDWIDTH):
+		for boxy in range(BOARDHEIGHT):
+			left, top = leftTopCoordsOfBox(boxx, boxy)
+			if not revealed[boxx][boxy]:
+				# Draw a covered box.
+				pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, BOXSIZE, BOXSIZE))
+			else:
+				# Draw the (revealed) icon.
+				shape, color = getShapeAndColor(board, boxx, boxy)
+				drawIcon(shape, color, boxx, boxy)
+
 def drawHighlightBox(boxx, boxy):
- left, top = leftTopCoordsOfBox(boxx, boxy)
- pygame.draw.rect(DISPLAYSURF, HIGHLIGHTCOLOR, (left - 5, top - 5,
-BOXSIZE + 10, BOXSIZE + 10), 4)
+	left, top = leftTopCoordsOfBox(boxx, boxy)
+	pygame.draw.rect(DISPLAYSURF, HIGHLIGHTCOLOR, (left - 5, top - 5, BOXSIZE + 10, BOXSIZE + 10), 4)
 def startGameAnimation(board):
  # Randomly reveal the boxes 8 at a time.
  coveredBoxes = generateRevealedBoxesData(False)
